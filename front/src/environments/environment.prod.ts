795cd399c4cecd6d53526634f214231b44697a62
standemdem <stan.demontmarin@gmail.com>
2019-07-04 11:08:27 +0200

    Help

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-
+  firebaseConfig: {
+    apiKey: "AIzaSyC9aa5Wehft1idJgNtRYtgLy-ceGyNfyNM",
+    authDomain: "monprojet-3cd36.firebaseapp.com",
+    databaseURL: "https://monprojet-3cd36.firebaseio.com",
+    projectId: "monprojet-3cd36",
+    storageBucket: "monprojet-3cd36.appspot.com",
+    messagingSenderId: "423255395541",
+    appId: "1:423255395541:web:399bf035ae590c99"
+  }
 };
